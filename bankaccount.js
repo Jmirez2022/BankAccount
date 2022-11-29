d3f510aecb6e284d885a49591db433a273c1b247
Jmirez2022 <Jazi.2000@hotmail.com>
2022-11-29 11:16:23 -0600

    finished

--- bankaccount.js
+++ bankaccount.js
@@ -18,13 +18,18 @@ class BankAccount {
     }
 
     charge(payee, amt){
+        let currentBalance = this.balance();
+        if(amt <= currentBalance) {
         let chargeTransaction = new Transaction(-amt, payee);
         this.transactions.push(chargeTransaction);
+        }
     }
 
     deposit(amt){
-        let depositTransaction = new Transaction(amt, this.owner);
-        this.transactions.push(depositTransaction);
+        if(amt > 0){
+            let depositTransaction = new Transaction(amt, this.owner);
+            this.transactions.push(depositTransaction);
+        }    
     }
 
 }
@@ -70,6 +75,20 @@ it('should create a new account correctly', function(){
        assert.equal(acct1.balance(), 150);
     });
 
+    it('should not allow charging to overdraft', function(){
+        let acct1 = new BankAccount('xx2020', "Joe Mama");
+       assert.equal(acct1.balance(), 0);
+       acct1.charge("Walmart", 50);
+       assert.equal(acct1.balance(), 0);
+    });
+
+    it('should allow a refund', function(){
+        let acct1 = new BankAccount('xx2020', "Joe Mama");
+       assert.equal(acct1.balance(), 0);
+       acct1.charge("Walmart", -50);
+       assert.equal(acct1.balance(), 50);
+    });
+
 });
 
     describe("#Testing transaction creation", function(){
@@ -80,6 +99,7 @@ it('should create a new account correctly', function(){
             assert.notEqual(t1.date, undefined);
             assert.notEqual(t1.date, null);  
         });
+
         it('Should create a transaction correctly for a charge', function(){
             let t1 = new Transaction(-50.50, "Walmart");
             assert.equal(t1.amount, -50.50);
@@ -87,7 +107,51 @@ it('should create a new account correctly', function(){
             assert.notEqual(t1.date, undefined);
             assert.notEqual(t1.date, null);
     });
-
-
 })
+
+    describe(" Transactrions and tests", function(){
+        let bigAccount = new BankAccount("12345678", "Will Smith");
+        it("test account created correctly", function(){
+            assert.equal("12345678", bigAccount.accountNumber);
+            assert.equal("Will Smith", bigAccount.owner);
+            assert.equal(bigAccount.balance(), 0);
+        });
+
+        it("test deposits", function(){
+            bigAccount.deposit(100); //100
+            assert.equal('Deposit', bigAccount.transactions[0].payee)
+            assert.equal(100, bigAccount.transactions[0].amount)          
+            bigAccount.deposit(50);  //150
+            bigAccount.deposit(-3); //150
+            bigAccount.deposit(80.50); //230.50
+            bigAccount.deposit(10000.50); //10,231
+            assert.equal(10231, bigAccount.balance());
+            bigAccount.charge("Clearout", 10231);
+            assert.equal(0, bigAccount.balance());
+        });
+
+        it("test charges", function(){
+            bigAccount.deposit(10000);
+            bigAccount.charge("Walmart", 50);  //9,950
+            bigAccount.charge("Wingstop", 75.25);  //9,874.75
+            bigAccount.charge("Best Buy", 550.70);  //9,324.05
+            bigAccount.charge("Gamestop", -63.20); //9,387.25
+            assert.equal(9387.25, bigAccount.balance());
+            assert.equal(10, bigAccount.transactions.length);
+        });
+
+        it("test overdraft", function(){
+            
+            bigAccount.charge("Walmart", 500000);
+            assert.equal(10, bigAccount.transactions.length);
+            assert.equal(9387.25, bigAccount.balance());
+        });
+
+        it("test a zero depsoit", function(){
+            bigAccount.deposit(0)
+            assert.equal(11, bigAccount.transactions.length);
+            assert.equal(9387.25, bigAccount.balance());
+        })
+       
+    });
 }
